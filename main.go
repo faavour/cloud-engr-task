2ba994b8824893c329ff38249702da44316682f4
faavour <favourjohn862@gmail.com>
2024-08-31 20:11:23 +0100

    request funtion

--- main.go
+++ main.go
@@ -2,9 +2,18 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
+	"net/http"
 	"time"
 )
 
 type TimeResponse struct {
 	Time string `json:"time"`
 }
+
+func getTime(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	currentTime := TimeResponse{Time: time.Now().UTC().Format(time.RFC3339)}
+	json.NewEncoder(w).Encode(currentTime)
+}
+
